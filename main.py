beae8f7b0dda7937ebd2b8adfecb474c25636caa
Rishab Kumar <rishabkumar7@gmail.com>
2023-05-04 13:58:41 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+import speedtest
+import time
+
+app = Flask(__name__)
+
+@app.route('/speedtest')
+def measure_speed():
+    speedtester = speedtest.Speedtest()
+    message = "Please wait while the speed test is in progress..."
+    print(message)
+    time.sleep(5)  # Simulate calculation time
+    download_speed = speedtester.download() / 10**6
+    upload_speed = speedtester.upload() / 10**6
+    result = {"download_speed": download_speed, "upload_speed": upload_speed}
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run()
