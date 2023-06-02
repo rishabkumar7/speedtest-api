965982d3f0a3b42652386574590b6724d500ec30
Rishab Kumar <rishabkumar7@gmail.com>
2023-06-02 14:07:38 -0400

    Added ping and ip functions

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import ping3
 from flask import Flask, jsonify, render_template, request
 import speedtest
 import time
@@ -11,7 +12,22 @@ def index():
 
 @app.route('/myip')
 def get_my_ip():
-    return jsonify({'ip': request.remote_addr}), 200
+    user_ip = request.remote_addr
+    ping_result = measure_ping(user_ip)
+    result = {"user_ip": user_ip, "ping_result": ping_result}
+    return jsonify(result)
+#f"Your public IP address is: {user_ip}\nPing Result: {ping_result}"
+
+def measure_ping(ip_address):
+    try:
+        response_time = ping3.ping(ip_address, timeout=2, unit="ms")
+        if response_time is not None:
+            result = f"Response Time: {response_time} ms"
+        else:
+            result = "No response"
+    except Exception as e:
+        result = str(e)
+    return result 
 
 @app.route('/speedtest')
 def measure_speed():
